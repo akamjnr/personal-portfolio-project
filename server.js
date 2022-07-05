500e90335b7066472b9372968a4af1cc96ed37f3
akamjnr <akamjnr@gmail.com>
2022-07-05 06:17:23 +0200

    fix OAuth bug

--- server.js
+++ server.js
@@ -89,7 +89,7 @@ const GOOGLE_CLIENT_SECRET = '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';
 passport.use(new GoogleStrategy({
     clientID: GOOGLE_CLIENT_ID,
     clientSecret: GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/callback"
+    callbackURL: "https://ak-personal-portfolio-project.herokuapp.com/api-docs"
   },
   function(accessToken, refreshToken, profile, done) {
       userProfile=profile;
