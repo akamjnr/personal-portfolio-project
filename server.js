67f04e1d935cdccb5a337bdf8a0aeebab9b250ef
akamjnr <akamjnr@gmail.com>
2022-07-05 07:06:36 +0200

    L08 Personal Assignment Completed

--- server.js
+++ server.js
@@ -1,28 +1,3 @@
-// const express = require('express');
-// const bodyParser = require('body-parser');
-// const mongodb = require('./db/connect');
-
-// const port = process.env.PORT || 8080;
-// const app = express();
-
-// app
-//   .use(bodyParser.json())
-//   .use((req, res, next) => {
-//     res.setHeader('Access-Control-Allow-Origin', '*');
-//     next();
-//   })
-//   .use('/', require('./routes'));
-
-// mongodb.initDb((err) => {
-//   if (err) {
-//     console.log(err);
-//   } else {
-//     app.listen(port);
-//     console.log(`Connected to DB and listening on ${port}`);
-//   }
-// });
-
-
 const express = require('express');
 const app = express();
 const session = require('express-session');
@@ -106,4 +81,3 @@ app.get('/auth/google/callback',
     // Successful authentication, redirect success.
     res.redirect('/success');
   });
-
